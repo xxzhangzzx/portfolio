a2c53e4942a68b1dfa6a5613a793a54cf84bb33e
xxzhangzzx <zhz194@ucsd.edu>
2026-04-17 22:12:55 -0700

    update

--- global.js
+++ global.js
@@ -1,48 +1,51 @@
 console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
+    return Array.from(context.querySelectorAll(selector));
 }
 
 const pages = [
-  { url: "", title: "Home" },
-  { url: "projects/", title: "Projects" },
-  { url: "contact/", title: "Contact" },
-  { url: "resume/", title: "Resume" },
-  { url: "https://github.com/xxzhangzzx", title: "GitHub" },
+    { url: "", title: "Home" },
+    { url: "projects/", title: "Projects" },
+    { url: "contact/", title: "Contact" },
+    { url: "resume/", title: "Resume" },
+    { url: "https://github.com/xxzhangzzx", title: "GitHub" },
 ];
 
 const BASE_PATH =
-  location.hostname === "localhost" || location.hostname === "127.0.0.1"
-    ? "/"
-    : "/portfolio/";
+    location.hostname === "localhost" || location.hostname === "127.0.0.1"
+        ? "/"
+        : "/portfolio/";
 
 let nav = document.createElement("nav");
 document.body.prepend(nav);
 
 for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
+    let url = p.url;
+    let title = p.title;
 
-  url = !url.startsWith("http") ? BASE_PATH + url : url;
+    url = !url.startsWith("http") ? BASE_PATH + url : url;
 
-  let a = document.createElement("a");
-  a.href = url;
-  a.textContent = title;
+    let a = document.createElement("a");
+    a.href = url;
+    a.textContent = title;
 
-  if (a.host === location.host && a.pathname === location.pathname) {
-    a.classList.add("current");
-  }
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add("current");
+    }
 
-  if (a.host !== location.host) {
-    a.target = "_blank";
-    a.rel = "noopener noreferrer";
+    if (a.host !== location.host) {
+        a.target = "_blank";
+        a.rel = "noopener noreferrer";
+    }
+
+    nav.append(a);
 }
 
 let navLinks = $$("nav a");
 
 let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
+    (a) => a.host === location.host && a.pathname === location.pathname
 );
 
 currentLink?.classList.add("current");
