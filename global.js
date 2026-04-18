3288ffe6ada6adf3c78a22d65f93f522515cbc93
xxzhangzzx <zhz194@ucsd.edu>
2026-04-17 22:39:24 -0700

    update

--- global.js
+++ global.js
@@ -42,10 +42,23 @@ for (let p of pages) {
     nav.append(a);
 }
 
-let navLinks = $$("nav a");
-
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
+document.body.insertAdjacentHTML(
+    "afterbegin",
+    `
+        <label class="color-scheme">
+            Theme:
+            <select>
+                <option value="light dark">Automatic</option>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+            </select>
+        </label>
+    `
 );
 
-currentLink?.classList.add("current");
+let select = document.querySelector(".color-scheme select");
+select.value = "light dark";
+
+select.addEventListener("input", function (event) {
+    document.documentElement.style.setProperty("color-scheme", event.target.value);
+});
