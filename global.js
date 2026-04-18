f387aa21d49008982bcb64b4234ea5bd517e79e3
xxzhangzzx <zhz194@ucsd.edu>
2026-04-17 21:39:46 -0700

    update

--- global.js
+++ global.js
@@ -4,6 +4,35 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+const pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "resume/", title: "Resume" },
+  { url: "https://github.com/xxzhangzzx", title: "GitHub" },
+];
+
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/";
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith("http") ? BASE_PATH + url : url;
+
+  let html = url.startsWith("http")
+    ? `<a href="${url}" target="_blank" rel="noopener noreferrer">${title}</a>`
+    : `<a href="${url}">${title}</a>`;
+
+  nav.insertAdjacentHTML("beforeend", html);
+}
+
 let navLinks = $$("nav a");
 
 let currentLink = navLinks.find(
