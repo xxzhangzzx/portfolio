77f55fb15c72a9680741d057c5199af87fed7616
xxzhangzzx <zhz194@ucsd.edu>
2026-04-24 23:45:34 -0700

    update

--- projects/projects.js
+++ projects/projects.js
@@ -3,5 +3,8 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
+const projectsTitle = document.querySelector('.projects-title');
+
+projectsTitle.textContent = `${projects.length} Projects`;
 
 renderProjects(projects, projectsContainer, 'h2');
