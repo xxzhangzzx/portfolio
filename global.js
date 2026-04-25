e4e524692b8acbdef18c344198d219c72d6ab4bd
xxzhangzzx <zhz194@ucsd.edu>
2026-04-25 00:29:49 -0700

    update

--- global.js
+++ global.js
@@ -139,3 +139,18 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     containerElement.appendChild(article);
   }
 }
+
+export async function fetchGitHubData(username) {
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch GitHub data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching GitHub data:', error);
+  }
+}
--- index.js
+++ index.js
@@ -4,5 +4,19 @@ const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
-
 renderProjects(latestProjects, projectsContainer, 'h2');
+
+const githubData = await fetchGitHubData('xxzhangzzx');
+
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+  profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers:</dt><dd>${githubData.followers}</dd>
+      <dt>Following:</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
+}
