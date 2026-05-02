6a421102294520fa405ddaa10392d94a815b615f
xxzhangzzx <zhz194@ucsd.edu>
2026-05-01 22:14:51 -0700

    update

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,4 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
@@ -8,3 +9,15 @@ const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `${projects.length} Projects`;
 
 renderProjects(projects, projectsContainer, 'h2');
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('#projects-pie-plot')
+  .append('path')
+  .attr('d', arc)
+  .attr('fill', 'red');
