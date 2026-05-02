d0efc491ca2de14a94a49eff9ae7ab308fae5cec
xxzhangzzx <zhz194@ucsd.edu>
2026-05-01 21:50:51 -0700

    update

--- global.js
+++ global.js
@@ -133,7 +133,10 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     article.innerHTML = `
       <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
-      <p>${project.description}</p>
+      <div>
+        <p>${project.description}</p>
+        <p class="project-year">${project.year}</p>
+      </div>
     `;
 
     containerElement.appendChild(article);
