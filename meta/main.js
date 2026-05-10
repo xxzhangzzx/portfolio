ab0bde19a30920bcb31b3a1f2ba12e0eab10f625
xxzhangzzx <zhz194@ucsd.edu>
2026-05-09 22:38:02 -0700

    update

--- meta/main.js
+++ meta/main.js
@@ -164,6 +164,15 @@ function renderScatterPlot(data, commits) {
         .domain([0, 24])
         .range([usableArea.bottom, usableArea.top]);
 
+    const [minLines, maxLines] = d3.extent(commits, (d) => d.totalLines);
+
+    const rScale = d3
+        .scaleSqrt()
+        .domain([minLines, maxLines])
+        .range([2, 30]);
+
+    const sortedCommits = d3.sort(commits, (d) => -d.totalLines);
+
     const gridlines = svg
         .append('g')
         .attr('class', 'gridlines')
@@ -196,12 +205,14 @@ function renderScatterPlot(data, commits) {
 
     dots
         .selectAll('circle')
-        .data(commits)
+        .data(sortedCommits)
         .join('circle')
         .attr('cx', (d) => xScale(d.datetime))
         .attr('cy', (d) => yScale(d.hourFrac))
-        .attr('r', 5)
+        .attr('r', (d) => rScale(d.totalLines))
+        .style('fill-opacity', 0.7)
         .on('mouseenter', (event, commit) => {
+            d3.select(event.currentTarget).style('fill-opacity', 1);
             renderTooltipContent(commit);
             updateTooltipVisibility(true);
             updateTooltipPosition(event);
@@ -209,7 +220,8 @@ function renderScatterPlot(data, commits) {
         .on('mousemove', (event) => {
             updateTooltipPosition(event);
         })
-        .on('mouseleave', () => {
+        .on('mouseleave', (event) => {
+            d3.select(event.currentTarget).style('fill-opacity', 0.7);
             updateTooltipVisibility(false);
         });
 }
