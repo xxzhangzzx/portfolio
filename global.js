09dda0317046cba0bb5c054b91cd37513fc47a98
xxzhangzzx <zhz194@ucsd.edu>
2026-05-09 19:58:57 -0700

    update

--- global.js
+++ global.js
@@ -25,6 +25,7 @@ const pages = [
     { url: "projects/", title: "Projects" },
     { url: "contact/", title: "Contact" },
     { url: "resume/", title: "Resume" },
+    { url: 'meta/', title: 'Meta' },
     { url: "https://github.com/xxzhangzzx", title: "GitHub" },
 ];
 
